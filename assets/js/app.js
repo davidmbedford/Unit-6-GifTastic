3097f7a30eac12a03a85e235a59112554850c59e
DavidmB <david.m.bedford1@gmail.com>
2019-03-28 16:08:43 -0400

    mar 28, succesfully retrieving api data via console log

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,27 +5,26 @@
 
 $(document).ready(function() {
 
-var topics = ["pork", "bork", "fork"];
+var topics = ["lions", "tigers", "bears"];
 
 // sets buttons to page
 function renderButtons() {
   $("#gif-buttons").empty();
 
   for (i = 0; i < topics.length; i++) {
-  //var topicName = topics[i];
-  //$("#gif-buttons").append("<button> " + topicName + " </button>");
-  //  };
-  var g = $("<button>");
-  g.addClass("gif");
-  g.attr("data-value", topics[i]);
-  g.text(topics[i]);
-  $("#gif-buttons").append(g);
+    //var topicName = topics[i];
+    //$("#gif-buttons").append("<button> " + topicName + " </button>");
+    //  };
+    var g = $("<button>");
+    g.addClass("gif-btn");
+    g.attr("data-value", topics[i]);
+    g.text(topics[i]);
+    $("#gif-buttons").append(g);
+    };
+
+  console.log(topics);
+
   };
-
-  console.log(topics)
-  };
-
-
 
 // provides functionality for submit(+) button
 $("#submit-btn").on("click", function(event) {
@@ -54,22 +53,26 @@ $("#remove-btn").on("click", function(event) {
   });
 
 // provides functionality for the gif-api buttons at the top eof the page
-$("<button>").on("click", function(event) {
+function displayGifs () {
 
   event.preventDefault();
 
-  var gif = $("#gif-input").val().trim();
+  var gif = $(this).attr("data-value");
 
-  var queryUrl = "https://api.giphy.com/v1/gifs/search?api_key=sQdRRctfFw20rFHJs2Hm49aWOfHWzHqY&q=" + gif + "&limit=10&offset=0&rating=G&lang=en"
+  var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + gif + "&api_key=sQdRRctfFw20rFHJs2Hm49aWOfHWzHqY&limit=10&offset=0&rating=G&lang=en"
 
     $.ajax({
       url: queryURL,
       method: "GET"
     }).then(function(response) {
+      console.log(response);
+
 
       });
 
-  });
+  };
+
+$(document).on("click", ".gif-btn", displayGifs);
 
 // call of array onstart
 renderButtons();
