abfc607668a2e7fd54d5afdcb5cb62ea435ed3ad
DavidmB <david.m.bedford1@gmail.com>
2019-03-27 23:41:53 -0400

    mar 27th, js step one

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,3 +4,20 @@
 //////////////////////////////////
 
 var topics = [];
+
+$("#submitBtn").on("click", function(event) {
+
+  event.preventDefault();
+
+  var gif = $("#gif-input").val().trim();
+
+  var queryUrl = "https://api.giphy.com/v1/gifs/search?api_key=sQdRRctfFw20rFHJs2Hm49aWOfHWzHqY&q=" + gif + "&limit=10&offset=0&rating=G&lang=en"
+
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  }).then(function(response) {
+
+  });
+
+});
